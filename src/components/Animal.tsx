97b5b31b7110d76485e90a8413c21fe02979e5c6
marv-ink <marvin96k@gmail.com>
2021-06-16 22:30:42 +0200

    able to add new values and save them to localStorage

--- src/components/Animal.tsx
+++ src/components/Animal.tsx
@@ -7,7 +7,7 @@ interface IAnimalParams {
 }
 export function AnimalDetails() {
   let { id } = useParams<IAnimalParams>();
-  let stateDefaultValue: AnimalSpecifics = {
+  let animalStateDefaultValue: AnimalSpecifics = {
     id: 0,
     name: '',
     latinName: '',
@@ -18,24 +18,47 @@ export function AnimalDetails() {
     isFed: false,
     lastFed: new Date(),
   };
-  const [animal, setAnimal] = useState(stateDefaultValue);
+  const [animal, setAnimal] = useState(animalStateDefaultValue);
+
+  // hämtar listan från localStorage
+  let animalsStateDefaultValue: AnimalSpecifics[] = JSON.parse(localStorage.getItem('Animals') || '[]');
+  const [animals, setAnimals] = useState(animalsStateDefaultValue);
+
+  //Ändrar id värde från string till number
+  const parsedParamsID: number = parseInt(id);
+
   useEffect(() => {
-    // hämtar listan från localStorage
-    let getDataFromLS: AnimalSpecifics[] = JSON.parse(localStorage.getItem('Animals') || '[]');
-    //Ändrar id värde från string till number
-    let parsedParamsID: number = parseInt(id);
     // Går igenom min lista från localStorage för att hitta ett object med samma id som finns i min URL för just det djuret
-    let animalOB = getDataFromLS.find((animalObject) => animalObject.id === parsedParamsID);
-    if (animalOB) {
-      setAnimal(animalOB);
+    let animalObj = animals.find((animalObject) => animalObject.id === parsedParamsID);
+    if (animalObj) {
+      setAnimal(animalObj);
     } else {
       console.log('localStorage is empty');
     }
-  }, [id]);
+  }, [id, parsedParamsID, animals]);
+
+  function feedAnimal() {
+    const updatedAnimal = [...animals];
+    for (let i = 0; i < updatedAnimal.length; i++) {
+      if (updatedAnimal[i].id === parsedParamsID) {
+        updatedAnimal[i].isFed = true;
+        updatedAnimal[i].lastFed = new Date();
+
+        localStorage.setItem('Animals', JSON.stringify(updatedAnimal));
+        setAnimal(updatedAnimal[i]);
+        setAnimals(updatedAnimal);
+      }
+    }
+  }
+
   return (
     <div>
+      <div></div>
       <h4>{animal.latinName}</h4>
       <img src={animal.imageUrl} alt={animal.name} />
+      <button onClick={feedAnimal}>Mata mig!</button>
+      <p>Djur är mattat: {animal.isFed ? 'Ja' : 'Nej'}</p>
+      <p>{animal.isFed.toString()}</p>
     </div>
   );
 }
